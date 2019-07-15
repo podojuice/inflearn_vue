da5cf14b76fcf1bed3bdaec1644ce476723f96d4
Yeohyungoo <hyungu0611@gmail.com>
2019-07-15 17:41:07 +0900

    first commit

---
+++ vue-news/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    hello
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  methods: {
+    fetchData() {
+      console.log('hello')
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
